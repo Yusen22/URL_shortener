55222ed464f7569c99c8f5bbd260f38c73cb1654
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-17 01:13:49 +0700

    connected mongoose and required and set up body-parser

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
